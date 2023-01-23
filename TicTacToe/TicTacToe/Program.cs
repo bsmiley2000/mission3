04efc0afb0ec7ea3c9dcd32684542faa092a2efa
= <zm275@byu.edu>
2023-01-23 16:02:02 -0700

    supporting functions are complete

--- TicTacToe/TicTacToe/Program.cs
+++ TicTacToe/TicTacToe/Program.cs
@@ -6,7 +6,18 @@ namespace TicTacToe
     {
         static void Main(string[] args)
         {
+            char[] BoardArray = new char[] {' ',' ',' ',' ',' ',' ',' ',' ',' '};
             Console.WriteLine("Hello World!");
+            Console.WriteLine("     |     |      ");
+            Console.WriteLine("  {0}  |  {1}  |  {2}", BoardArray[0], BoardArray[1], BoardArray[2]);
+            Console.WriteLine("_____|_____|_____ ");
+            Console.WriteLine("     |     |      ");
+            Console.WriteLine("  {0}  |  {1}  |  {2}", BoardArray[3], BoardArray[4], BoardArray[5]);
+            Console.WriteLine("_____|_____|_____ ");
+            Console.WriteLine("     |     |      ");
+            Console.WriteLine("  {0}  |  {1}  |  {2}", BoardArray[6], BoardArray[7], BoardArray[8]);
+            Console.WriteLine("     |     |      ");
+
         }
     }
 }
