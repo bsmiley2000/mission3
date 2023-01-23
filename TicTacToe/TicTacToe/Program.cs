fac678b7341d7972b27cfbfd1e863b17736f9276
Brock Smiley <bs539@byu.edu>
2023-01-23 16:23:08 -0700

    finished driver

--- TicTacToe/TicTacToe/Program.cs
+++ TicTacToe/TicTacToe/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Threading;
 
 namespace TicTacToe
 {
@@ -6,7 +7,52 @@ namespace TicTacToe
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            // Welcome user to the game
+            Console.WriteLine("Welcome to a fantastic game of Tic Tac Toe!" + "\n");
+
+            // Init the player's turn
+            int player = 1;
+          
+
+            // Init our array of 9 positions
+            string[] gameBoard = new string[10];
+
+            bool gameOver = false;
+            int userInput = 0;
+
+
+            while (player < 10)
+            {
+
+                if (player % 2 == 0)
+                {
+                    Console.Write("Player 2 play: ");
+                    userInput = Convert.ToInt32(Console.ReadLine());
+
+                    gameBoard[userInput] = "O";
+
+                    player++;
+                }
+
+
+                else
+                {
+                    Console.Write("Player 1 play: ");
+                    userInput = Convert.ToInt32(Console.ReadLine());
+
+                    gameBoard[userInput] = "X";
+
+                    player++;
+                }
+
+            }
+
+           // for (int i = 0; i < gameBoard.Length; i++ )
+           // {
+           //     Console.WriteLine(gameBoard[i]);
+           // }
+
+
         }
     }
 }
