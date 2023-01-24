a2eb27d3d180f0a4341977848549650661118332
Brock Smiley <bs539@byu.edu>
2023-01-23 17:33:39 -0700

    merged

--- TicTacToe/TicTacToe/Program.cs
+++ TicTacToe/TicTacToe/Program.cs
@@ -9,43 +9,59 @@ namespace TicTac
     {
         static void Main(string[] args)
         {
-            // Welcome user to the game
-            Console.WriteLine("Welcome to a fantastic game of Tic Tac Toe!" + "\n");
+            
+
             // Init the player's turn
             int player = 1;
+
             // Init our array of 9 positions
-            string[] gameBoard = new string[10];
+            char[] gameBoard = {'0', '1', '2', '3', '4', '5', '6', '7', '8', '9' };
             bool gameOver = false;
             int userInput = 0;
 
             Supporting s = new Supporting();
 
+            
+
             while (player < 10)
             {
+
+                // Welcome user to the game
+                Console.WriteLine("Welcome to a fantastic game of Tic Tac Toe!" + "\n");
+
+                s.PrintBoard(gameBoard);
+
                 if (player % 2 == 0)
                 {
                     Console.Write("Player 2 play: ");
                     userInput = Convert.ToInt32(Console.ReadLine());
-                    gameBoard[userInput] = "O";
+                    gameBoard[userInput] = Convert.ToChar("X");
                     player++;
                 }
                 else
                 {
                     Console.Write("Player 1 play: ");
                     userInput = Convert.ToInt32(Console.ReadLine());
-                    gameBoard[userInput] = "X";
+                    gameBoard[userInput] = Convert.ToChar("O");
                     player++;
                 }
+
+                s.PrintBoard(gameBoard);
+
+                char winner = s.DeclareWinner(gameBoard);
+
+                Console.Clear();
+
+              //  if (winner == 'D')
+             //   {
+              //      Console.WriteLine("CAT");
+               // }
+             
             }
 
-            s.PrintBoard(gameBoard);
 
 
             
-            // for (int i = 0; i < gameBoard.Length; i++ )
-            // {
-            //     Console.WriteLine(gameBoard[i]);
-            // }
         }
     }
 }
--- TicTacToe/TicTacToe/supporting.cs
+++ TicTacToe/TicTacToe/supporting.cs
@@ -9,13 +9,13 @@ namespace TicTac
         public void PrintBoard(char[] BoardArray)
         {
             Console.WriteLine("     |     |      ");
-            Console.WriteLine("  {0}  |  {1}  |  {2}", BoardArray[0], BoardArray[1], BoardArray[2]);
+            Console.WriteLine("  {0}  |  {1}  |  {2}", BoardArray[1], BoardArray[2], BoardArray[3]);
             Console.WriteLine("_____|_____|_____ ");
             Console.WriteLine("     |     |      ");
-            Console.WriteLine("  {0}  |  {1}  |  {2}", BoardArray[3], BoardArray[4], BoardArray[5]);
+            Console.WriteLine("  {0}  |  {1}  |  {2}", BoardArray[4], BoardArray[5], BoardArray[6]);
             Console.WriteLine("_____|_____|_____ ");
             Console.WriteLine("     |     |      ");
-            Console.WriteLine("  {0}  |  {1}  |  {2}", BoardArray[6], BoardArray[7], BoardArray[8]);
+            Console.WriteLine("  {0}  |  {1}  |  {2}", BoardArray[7], BoardArray[8], BoardArray[9]);
             Console.WriteLine("     |     |      ");
 
         }
@@ -25,67 +25,70 @@ namespace TicTac
            if ((BoardArray[0] == 'X') & (BoardArray[1] == 'X') & (BoardArray[2] == 'X'))
             {
                 result = 'X';
+                Console.WriteLine("Player 1 wins!");
+
             }
-            if ((BoardArray[0] == 'O') & (BoardArray[1] == 'O') & (BoardArray[2] == 'O'))
+            else if ((BoardArray[0] == 'O') & (BoardArray[1] == 'O') & (BoardArray[2] == 'O'))
             {
                 result = 'O';
             }
-            if ((BoardArray[3] == 'X') & (BoardArray[4] == 'X') & (BoardArray[5] == 'X'))
+            else if ((BoardArray[3] == 'X') & (BoardArray[4] == 'X') & (BoardArray[5] == 'X'))
             {
                 result = 'X';
             }
-            if ((BoardArray[3] == 'O') & (BoardArray[4] == 'O') & (BoardArray[5] == 'O'))
+            else if ((BoardArray[3] == 'O') & (BoardArray[4] == 'O') & (BoardArray[5] == 'O'))
             {
                 result = 'O';
             }
-            if ((BoardArray[6] == 'X') & (BoardArray[7] == 'X') & (BoardArray[8] == 'X'))
+            else if ((BoardArray[6] == 'X') & (BoardArray[7] == 'X') & (BoardArray[8] == 'X'))
             {
                 result = 'X';
             }
-            if ((BoardArray[6] == 'O') & (BoardArray[7] == 'O') & (BoardArray[8] == 'O'))
+            else if ((BoardArray[6] == 'O') & (BoardArray[7] == 'O') & (BoardArray[8] == 'O'))
             {
                 result = 'O';
             }
-            if ((BoardArray[0] == 'X') & (BoardArray[3] == 'X') & (BoardArray[6] == 'X'))
+            else if ((BoardArray[0] == 'X') & (BoardArray[3] == 'X') & (BoardArray[6] == 'X'))
             {
                 result = 'X';
             }
-            if ((BoardArray[0] == 'O') & (BoardArray[3] == 'O') & (BoardArray[6] == 'O'))
+            else if ((BoardArray[0] == 'O') & (BoardArray[3] == 'O') & (BoardArray[6] == 'O'))
             {
                 result = 'O';
             }
-            if ((BoardArray[1] == 'X') & (BoardArray[4] == 'X') & (BoardArray[7] == 'X'))
+            else if ((BoardArray[1] == 'X') & (BoardArray[4] == 'X') & (BoardArray[7] == 'X'))
             {
                 result = 'X';
             }
-            if ((BoardArray[1] == 'O') & (BoardArray[4] == 'O') & (BoardArray[7] == 'O'))
+            else if ((BoardArray[1] == 'O') & (BoardArray[4] == 'O') & (BoardArray[7] == 'O'))
             {
                 result = 'O';
             }
-            if ((BoardArray[2] == 'X') & (BoardArray[5] == 'X') & (BoardArray[8] == 'X'))
+            else if ((BoardArray[2] == 'X') & (BoardArray[5] == 'X') & (BoardArray[8] == 'X'))
             {
                 result = 'X';
             }
-            if ((BoardArray[2] == 'O') & (BoardArray[5] == 'O') & (BoardArray[8] == 'O'))
+            else if ((BoardArray[2] == 'O') & (BoardArray[5] == 'O') & (BoardArray[8] == 'O'))
             {
                 result = 'O';
             }
-            if ((BoardArray[0] == 'X') & (BoardArray[4] == 'X') & (BoardArray[8] == 'X'))
+            else if ((BoardArray[0] == 'X') & (BoardArray[4] == 'X') & (BoardArray[8] == 'X'))
             {
                 result = 'X';
             }
-            if ((BoardArray[0] == 'O') & (BoardArray[4] == 'O') & (BoardArray[8] == 'O'))
+            else if ((BoardArray[0] == 'O') & (BoardArray[4] == 'O') & (BoardArray[8] == 'O'))
             {
                 result = 'O';
             }
-            if ((BoardArray[2] == 'X') & (BoardArray[4] == 'X') & (BoardArray[6] == 'X'))
+            else if ((BoardArray[2] == 'X') & (BoardArray[4] == 'X') & (BoardArray[6] == 'X'))
             {
                 result = 'X';
             }
-            if ((BoardArray[2] == 'O') & (BoardArray[4] == 'O') & (BoardArray[6] == 'O'))
+            else if ((BoardArray[2] == 'O') & (BoardArray[4] == 'O') & (BoardArray[6] == 'O'))
             {
                 result = 'O';
             }
+            
             return result;
         }
     }
