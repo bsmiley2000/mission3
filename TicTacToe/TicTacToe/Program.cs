eb1ece579cf08183bd51eb083d54b88a388971c9
Brock Smiley <bs539@byu.edu>
2023-01-24 15:31:13 -0700

    finished

--- TicTacToe/TicTacToe/Program.cs
+++ TicTacToe/TicTacToe/Program.cs
@@ -9,53 +9,45 @@ namespace TicTac
     {
         static void Main(string[] args)
         {
-            
-
             // Init the player's turn
             int player = 1;
 
             // Init our array of 9 positions
             char[] gameBoard = {'0', '1', '2', '3', '4', '5', '6', '7', '8', '9' };
-            bool gameOver = false;
             int userInput = 0;
 
             Supporting s = new Supporting();
 
-            
+            char result = s.DeclareWinner(gameBoard);
 
-            while (player < 10)
+            while (result != 1)
             {
 
                 // Welcome user to the game
                 Console.WriteLine("Welcome to a fantastic game of Tic Tac Toe!" + "\n");
 
                 s.PrintBoard(gameBoard);
+                s.DeclareWinner(gameBoard);
 
                 if (player % 2 == 0)
                 {
-                    Console.Write("Player 2 play: ");
+                    Console.Write("\n" + "Player 2 play: ");
                     userInput = Convert.ToInt32(Console.ReadLine());
                     gameBoard[userInput] = Convert.ToChar("X");
                     player++;
                 }
                 else
                 {
-                    Console.Write("Player 1 play: ");
+                    Console.Write("\n" + "Player 1 play: ");
                     userInput = Convert.ToInt32(Console.ReadLine());
                     gameBoard[userInput] = Convert.ToChar("O");
                     player++;
                 }
 
-                s.PrintBoard(gameBoard);
-
-                char winner = s.DeclareWinner(gameBoard);
 
                 Console.Clear();
 
-              //  if (winner == 'D')
-             //   {
-              //      Console.WriteLine("CAT");
-               // }
+
              
             }
 
--- TicTacToe/TicTacToe/supporting.cs
+++ TicTacToe/TicTacToe/supporting.cs
@@ -2,10 +2,15 @@
 using System.Collections.Generic;
 using System.Text;
 
+// Zach, Max, Brock, Katherine
+// This program plays tic tac toe and does it between two classes
+// this program allows the players to keep playing if even a winner has been declared
+
 namespace TicTac
 {
     class Supporting
     {
+        // Method that prints the board along with the user choice
         public void PrintBoard(char[] BoardArray)
         {
             Console.WriteLine("     |     |      ");
@@ -19,76 +24,86 @@ namespace TicTac
             Console.WriteLine("     |     |      ");
 
         }
+
+        // Method that declares the winner or if there is a draw
         public char DeclareWinner(char[] BoardArray)
         {
-           char result = 'D';
-           if ((BoardArray[0] == 'X') & (BoardArray[1] == 'X') & (BoardArray[2] == 'X'))
-            {
-                result = 'X';
-                Console.WriteLine("Player 1 wins!");
+            char result = 'a';
+           
 
+            if ((BoardArray[1] == 'X') & (BoardArray[2] == 'X') & (BoardArray[3] == 'X'))
+            {               
+                Console.WriteLine("Player 2 Wins!");
             }
-            else if ((BoardArray[0] == 'O') & (BoardArray[1] == 'O') & (BoardArray[2] == 'O'))
+            else if ((BoardArray[1] == 'O') & (BoardArray[2] == 'O') & (BoardArray[3] == 'O'))
             {
-                result = 'O';
+                Console.WriteLine("Player 1 Wins!");
             }
-            else if ((BoardArray[3] == 'X') & (BoardArray[4] == 'X') & (BoardArray[5] == 'X'))
+            else if ((BoardArray[4] == 'X') & (BoardArray[5] == 'X') & (BoardArray[6] == 'X'))
             {
-                result = 'X';
+                Console.WriteLine("Player 2 Wins!");
             }
-            else if ((BoardArray[3] == 'O') & (BoardArray[4] == 'O') & (BoardArray[5] == 'O'))
+            else if ((BoardArray[4] == 'O') & (BoardArray[5] == 'O') & (BoardArray[6] == 'O'))
             {
-                result = 'O';
+                Console.WriteLine("Player 1 Wins!");
             }
-            else if ((BoardArray[6] == 'X') & (BoardArray[7] == 'X') & (BoardArray[8] == 'X'))
+            else if ((BoardArray[7] == 'X') & (BoardArray[8] == 'X') & (BoardArray[9] == 'X'))
             {
-                result = 'X';
+                Console.WriteLine("Player 2 Wins!");
             }
-            else if ((BoardArray[6] == 'O') & (BoardArray[7] == 'O') & (BoardArray[8] == 'O'))
+            else if ((BoardArray[7] == 'O') & (BoardArray[8] == 'O') & (BoardArray[9] == 'O'))
             {
-                result = 'O';
-            }
-            else if ((BoardArray[0] == 'X') & (BoardArray[3] == 'X') & (BoardArray[6] == 'X'))
-            {
-                result = 'X';
-            }
-            else if ((BoardArray[0] == 'O') & (BoardArray[3] == 'O') & (BoardArray[6] == 'O'))
-            {
-                result = 'O';
+                Console.WriteLine("Player 1 Wins!");
             }
             else if ((BoardArray[1] == 'X') & (BoardArray[4] == 'X') & (BoardArray[7] == 'X'))
             {
-                result = 'X';
+                Console.WriteLine("Player 2 Wins!");
             }
-            else if ((BoardArray[1] == 'O') & (BoardArray[4] == 'O') & (BoardArray[7] == 'O'))
+            else if ((BoardArray[2] == 'O') & (BoardArray[4] == 'O') & (BoardArray[7] == 'O'))
             {
-                result = 'O';
+                Console.WriteLine("Player 1 Wins!");
             }
             else if ((BoardArray[2] == 'X') & (BoardArray[5] == 'X') & (BoardArray[8] == 'X'))
             {
-                result = 'X';
+                Console.WriteLine("Player 2 Wins!");
             }
             else if ((BoardArray[2] == 'O') & (BoardArray[5] == 'O') & (BoardArray[8] == 'O'))
             {
-                result = 'O';
+                Console.WriteLine("Player 1 Wins!");
             }
-            else if ((BoardArray[0] == 'X') & (BoardArray[4] == 'X') & (BoardArray[8] == 'X'))
+            else if ((BoardArray[3] == 'X') & (BoardArray[6] == 'X') & (BoardArray[9] == 'X'))
             {
-                result = 'X';
+                Console.WriteLine("Player 2 Wins!");
             }
-            else if ((BoardArray[0] == 'O') & (BoardArray[4] == 'O') & (BoardArray[8] == 'O'))
+            else if ((BoardArray[3] == 'O') & (BoardArray[6] == 'O') & (BoardArray[9] == 'O'))
             {
-                result = 'O';
+                Console.WriteLine("Player 1 Wins!");
             }
-            else if ((BoardArray[2] == 'X') & (BoardArray[4] == 'X') & (BoardArray[6] == 'X'))
+            else if ((BoardArray[1] == 'X') & (BoardArray[5] == 'X') & (BoardArray[9] == 'X'))
             {
-                result = 'X';
+                Console.WriteLine("Player 2 Wins!");
             }
-            else if ((BoardArray[2] == 'O') & (BoardArray[4] == 'O') & (BoardArray[6] == 'O'))
+            else if ((BoardArray[1] == 'O') & (BoardArray[5] == 'O') & (BoardArray[9] == 'O'))
             {
-                result = 'O';
+                Console.WriteLine("Player 1 Wins!");
             }
-            
+            else if ((BoardArray[3] == 'X') & (BoardArray[5] == 'X') & (BoardArray[7] == 'X'))
+            {
+                Console.WriteLine("Player 2 Wins!");
+            }
+            else if ((BoardArray[3] == 'O') & (BoardArray[5] == 'O') & (BoardArray[7] == 'O'))
+            {
+                Console.WriteLine("Player 1 Wins!");
+            }
+            else if (BoardArray[1] != '1' && BoardArray[2] != '2' && BoardArray[3] != '3' && BoardArray[4] != '4' && BoardArray[5] != '5' && BoardArray[6] != '6' && BoardArray[7] != '7' && BoardArray[8] != '8' && BoardArray[9] != '9')
+            {
+                Console.WriteLine("Looks like it's a CAT!");
+            }
+            else
+            {
+                result = 'a';
+            }
+
             return result;
         }
     }
